0cbe0d0b7c09824a22d0a8cc0a1b2d86bbaafbf2
Alejandro <aperalva@gmail.com>
2024-10-19 16:39:57 +0100

    3.15: Base de datos de la Agenda Telefónica, paso 3

--- index.js
+++ index.js
@@ -67,15 +67,7 @@ app.get('/api/persons/:id', (request, response, next) => {
       }
     })
     .catch(error => next(error))
-});
-
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    person = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-  })
+})
 
 const generateId = () => {
     return Math.floor(Math.random() * 1000000)
@@ -98,7 +90,8 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({ 
             error: 'name must be unique' 
         })
-      }*/
+      }
+    */
     const person = new Person({
       name: body.name,
       number: body.number
@@ -108,6 +101,32 @@ app.post('/api/persons', (request, response) => {
       response.json(savedPerson)
     })
   })
+
+  app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  app.use(errorHandler)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
