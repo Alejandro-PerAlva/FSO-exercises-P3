245d3b4d25cd13c196d0906581a303196d5206ca
Alejandro <aperalva@gmail.com>
2024-10-19 16:40:39 +0100

    3.16: Base de datos de la Agenda Telefónica, paso 4

--- index.js
+++ index.js
@@ -118,11 +118,9 @@ app.post('/api/persons', (request, response) => {
 
   const errorHandler = (error, request, response, next) => {
     console.error(error.message)
-  
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
     } 
-  
     next(error)
   }
   
