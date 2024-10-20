e1137dd944735b0e7be202c0a0c897bcadeddc22
Alejandro <aperalva@gmail.com>
2024-10-20 12:15:49 +0100

    3.22: Configuración de Lint

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ morgan.token('body', (req) => {
   })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-  
 
-/*let persons = [
-  {
+
+let persons = [
+  /*{
     id: 1,
     name: "Arto Hellas",
     number: "040-123456"
@@ -38,9 +38,9 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
     id: 4,
     name: "Mary Poppendieck",
     number: "39-23-6423122"
-  }
+  }*/
 ]
-*/
+
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -57,7 +57,7 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
-  
+
   Person.findById(id)
     .then(person => {
       if (person) {
@@ -73,8 +73,8 @@ app.get('/api/persons/:id', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
-        return response.status(400).json({ 
-            error: 'name or number is missing' 
+        return response.status(400).json({
+            error: 'name or number is missing'
         })
     }
 
@@ -99,17 +99,18 @@ app.get('/api/persons/:id', (request, response, next) => {
   app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
       .then(result => {
+        console.log(result)
         response.status(204).end()
       })
       .catch(error => next(error))
   })
 
   app.put('/api/persons/:id', (request, response, next) => {
-    const { name, number} = request.body
+    const { name, number } = request.body
 
     Person.findByIdAndUpdate(
       request.params.id,
-      { name, number}, 
+      { name, number },
       { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
           response.json(updatedPerson)
@@ -121,7 +122,7 @@ app.get('/api/persons/:id', (request, response, next) => {
   const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }
-  
+
   app.use(unknownEndpoint)
 
   const errorHandler = (error, request, response, next) => {
@@ -130,10 +131,10 @@ app.get('/api/persons/:id', (request, response, next) => {
       return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
       return response.status(400).json({ error: error.message })
-    } 
+    }
     next(error)
   }
-  
+
   app.use(errorHandler)
 
 const PORT = process.env.PORT || 3000
